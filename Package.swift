13ddbcc26c45c894de14b7bca73f33d74d14f1ed
Alexander Ignition <izh.sever@gmail.com>
2024-12-22 14:54:36 +0300

    Swift 5.9

--- Package.swift
+++ Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SQLime",
+    platforms: [
+        .macOS(.v12),
+        .iOS(.v12),
+    ],
     products: [
         .library(
             name: "SQLime",
